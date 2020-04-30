5e364208a97e064da07bbdd91f37f5e85ed69c56
Michiel Snoek <michiel.snoek@gmail.com>
2020-04-30 13:33:49 +0200

    added the remaining iterations for the lab

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   } 
 
   showMeFirstFiveContacts = () => {
-    var slicedContacts = this.state.contacts.slice(0,5)
+    let slicedContacts = this.state.contacts.splice(0,5)
     console.log(Math.random() * Contacts.length)
     this.setState({contacts: slicedContacts})
   }
@@ -21,9 +21,21 @@ export default class App extends Component {
   }
 
   addRandomContact = () => {
-    console.log(this.slicedContacts)
-    this.slicedContacts.push(Contacts[Math.floor(Math.random()* Contacts.length)])
-    this.setState({contacts: this.slicedContacts})
+    let initialState = this.state.contacts
+    initialState.push(Contacts[Math.floor(Math.random()* Contacts.length)])
+    this.setState({contacts: initialState})
+  }
+
+  sortName = () => {
+    let initialState = this.state.contacts
+    initialState.sort((a,b) => a.name < b.name ? -1 : 1)
+    this.setState({contacts: initialState})
+  }
+
+  sortpopularity = () => {
+    let initialState = this.state.contacts
+    initialState.sort((a,b) => a.popularity - b.popularity)
+    this.setState({contacts: initialState})
   }
 
   render() {
@@ -33,6 +45,8 @@ export default class App extends Component {
         <button onClick={this.showMeFirstFiveContacts}>Show only six contacts</button>
         <button onClick={this.showMeAllContacts}>Show all contacts</button>
         <button onClick={this.addRandomContact}>Add a random contact</button>
+        <button onClick={this.sortName}>sort by name</button>
+        <button onClick={this.sortpopularity}>sort by popularity</button>
         {this.state.contacts.map(contact => (
         <ContactsTwo key={contact.id}
         popularity={contact.popularity}
